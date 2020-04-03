219d4f30ad608f0190c181f9d73e539cfa2b26c4
aty26136 <53354396+aty26136@users.noreply.github.com>
2020-04-04 01:20:53 +0800

    在小字一组随机生成一段旋律

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -16,12 +16,9 @@ def play_note(note, length, track, base_num=0, delay=0, velocity=1.0, channel=0)
     track.append(Message('note_off', note=base_note + base_num*12 + sum(major_notes[0:note]), velocity=round(64*velocity), time=round(meta_time*length), channel=channel))
 
 #0.5=八分音符
-
-#random.ranint(1,10)
-my_list = [0.5, 1, 1.5]
-#print(random.choice(my_list))
+my_list = [0.5, 1, 1.5] #分别代表八分音符，四分音符以及带附点的四分音符
 
 for v in range(1,100):
-    play_note(random.randint(1,7), random.choice(my_list), track)
+    play_note(random.randint(1,7), random.choice(my_list), track) #仅仅在小字一组生成旋律
 
 mid.save('newSong.mid')
